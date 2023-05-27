de6c2f5926bd930e58caf010e104b57523615430
JaveriaIrfan686 <76628927+JaveriaIrfan686@users.noreply.github.com>
2023-05-27 17:44:29 +0500

    add another user participant

---
+++ AnotherUserPart.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import PollApp from "./UserParticipation";
+function UserTwo() {
+
+    
+    
+    return(
+        <>
+           <button onClick={()=>{<PollApp/>}}>Another User</button>
+        </>
+    )
+}
+
+export default UserTwo;
